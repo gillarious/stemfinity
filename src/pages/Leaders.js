a5f210c82caa0c0b92bb6e9d4f77fa2532e0bbf8
Valexa Orelien <vorelien@stanford.edu>
2020-08-02 12:42:49 -0400

    added title to eleaders.js

--- src/pages/Leaders.js
+++ src/pages/Leaders.js
@@ -1,22 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Grid, Container, makeStyles, Paper } from '@material-ui/core/';
 import '../App.css';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+      flexGrow: 1,
+  },
+  paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+  },
+}));
+
 export default function Leaders() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="Home">
+    <Container>
+        <Grid container spacing={10}>
+            <Grid item xs={12} className="title">
+                <Paper className={classes.paper}>Variables (Page 2)</Paper>
+            </Grid>
+        </Grid>
+        <Grid container spacing ={10}>
+          
+        </Grid>
+    </Container>
+</div>
   );
 }
