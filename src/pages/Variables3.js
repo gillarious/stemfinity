6ab91d1551cc6f3ce8bcf48c76445bbe59212f40
Governess <simps422@umn.edu>
2020-08-02 17:14:53 -0500

    add coding problem and multiple choice options, add links to buttons (will change later)

--- src/pages/Variables3.js
+++ src/pages/Variables3.js
@@ -5,6 +5,15 @@ import Button from '@material-ui/core/Button';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import { Link } from 'react-router-dom';
+import { render } from "react-dom";
+import prompt from '../assets/prompt.png';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormLabel from '@material-ui/core/FormLabel';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -31,6 +40,20 @@ const button = makeStyles(theme => ({
     bottom: 50,
     left: 50,
     textAlign: 'left'
+  },
+
+  footerI: {
+    position: 'fixed',
+    bottom: 150,
+    left: 100,
+    textAlign: 'left'
+  },
+
+  footerA: {
+    position: 'fixed',
+    bottom: 185,
+    right: 225,
+    textAlign: 'left'
   }
 }));
 
@@ -49,17 +72,58 @@ export default function Variables3() {
     const classes = useStyles();
     const loc = button();
 
+    const [value, setValue] = React.useState('');
+    const [error, setError] = React.useState(false);
+    const [helperText, setHelperText] = React.useState('');
+    
+    const handleRadioChange = (event) => {
+      setValue(event.target.value);
+      setHelperText(' ');
+      setError(false);
+    };
+    
+    const handleSubmit = (event) => {
+      event.preventDefault();
+    
+      if (value === 'best') {
+        setHelperText('You got it!');
+        setError(false);
+      } else if (value === 'worst') {
+        setHelperText('Sorry, wrong answer!');
+        setError(true);
+      } else if (value === 'meh') {
+        setHelperText('Sorry, wrong answer!');
+        setError(true);
+      } else if (value === 'yikes') {
+        setHelperText('Sorry, wrong answer!');
+        setError(true);
+      } else {
+        setHelperText('Please select an option.');
+        setError(true);
+      }
+    };
+
+
     return (
-    <div className="App-header">
+    <div className="Variables-header">
         <Container>
-            <Grid container spacing={10}>
+            <Grid container spacing={9}>
                 <Grid item xs={12}>
-                    <Paper className={classes.paper}>STEMfinity & Beyond</Paper>
+                    <Paper className={classes.paper}>Lesson: Variables (Review)</Paper>
+                </Grid>
+                <Grid item xs={12}>
+                    <Paper className={classes.paper}>Let's review what you learned!</Paper>
+                </Grid>
+                <Grid item xs={8}>
+                    <Paper className={loc.footerI}><img src={prompt} alt="prompt"/></Paper>
                 </Grid>
             </Grid>
         </Container>
 
+
     <div className="button-left">
+    <Paper className={loc.footerI}><img src={prompt} alt="prompt"/></Paper>
+
       <MuiThemeProvider theme={theme}>
         <Button
           variant="contained"
@@ -67,10 +131,28 @@ export default function Variables3() {
           className={loc.footerR}
           endIcon={<ArrowForwardIosIcon />}
         >
-        Next
+        <Link to="/VariablesSummary">Next</Link>
         </Button></MuiThemeProvider></div>
 
       <div className="button-right">
+
+      <form onSubmit={handleSubmit}>
+      <FormControl component="fieldset" error={error} className={loc.footerA}>
+        <FormLabel component="legend">What will be the output?</FormLabel>
+        <RadioGroup aria-label="quiz" name="quiz" value={value} onChange={handleRadioChange}>
+          <FormControlLabel value="best" control={<Radio />} label="A: 18" />
+          <FormControlLabel value="yikes" control={<Radio />} label="B: 17" />
+          <FormControlLabel value="meh" control={<Radio />} label="C: 19" />
+          <FormControlLabel value="worst" control={<Radio />} label="D: None of the Above" />
+        </RadioGroup>
+        <FormHelperText>{helperText}</FormHelperText>
+        <Button type="submit" variant="outlined" color="primary" className={classes.button}>
+          Check Answer
+        </Button>
+      </FormControl>
+    </form>
+
+
         <MuiThemeProvider theme={theme}>
         <Button
           variant="contained"
@@ -78,7 +160,7 @@ export default function Variables3() {
           className={loc.footerL}
           startIcon={<ArrowBackIosIcon />}
         >
-        Back
+        <Link to="/Variables2">Back</Link>
         </Button>
       </MuiThemeProvider></div>
     </div>
