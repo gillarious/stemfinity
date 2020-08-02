44c5514348fa5f73ef8995c449cee0063cf4b3f4
Governess <simps422@umn.edu>
2020-08-02 12:20:13 -0500

    add additional button, add icons and color styling

--- src/pages/Variables3.js
+++ src/pages/Variables3.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { Box, Grid, Container, makeStyles, Paper } from '@material-ui/core/';
 import '../App.css';
 import Button from '@material-ui/core/Button';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -15,6 +17,14 @@ const useStyles = makeStyles((theme) => ({
         color: theme.palette.text.secondary,
     },
 }));
+
+const theme = createMuiTheme({
+  palette: {
+      secondary: {
+          main: '#FFB5EA'
+      }
+    },
+});
 
 export default function Variables3() {
     const classes = useStyles();
@@ -28,19 +38,29 @@ export default function Variables3() {
                 </Grid>
             </Grid>
         </Container>
+      <MuiThemeProvider theme={theme}>
+        <Button
+          variant="outlined"
+          color = "secondary"
+          className={classes.button}
+          endIcon={<ArrowForwardIosIcon />}
+        >
+        Next
+        </Button>
 
         <Button
-  variant="outlined"
-  color="secondary"
-  className={classes.button}
-  startIcon={<ArrowForwardIosIcon />}
->
-  Next
-</Button>
-</div>
+          variant="outlined"
+          color = "secondary"
+          className={classes.button}
+          startIcon={<ArrowBackIosIcon />}
+        >
+        Back
+        </Button>
+      </MuiThemeProvider>
+    </div>
 
   );
-    }
+}
 
 
 {/*export default Variables3; */}
