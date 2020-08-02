f0f45f354f0ba083bd789014cf8084d5f877fde0
Stephanie Marie Estanislao <estaniss@uci.edu>
2020-08-02 14:31:58 -0700

    Added Conditionals, Variables0, and Variables1

---
+++ src/pages/Variables1.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Box, Grid, Container, makeStyles, Paper } from '@material-ui/core/';
+import '../App.css';
+import Button from '@material-ui/core/Button';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}));
+
+const theme = createMuiTheme({
+    palette: {
+        secondary: {
+            main: '#FFB5EA'
+        }
+    },
+});
+
+export default function Variables1() {
+    const classes = useStyles();
+
+    return (
+        <div className="App-header">
+            <Container>
+                <Grid container spacing={10}>
+                    <Grid item xs={12}>
+                        <Paper className={classes.paper}>STEMfinity & Beyond</Paper>
+                    </Grid>
+                </Grid>
+            </Container>
+            <MuiThemeProvider theme={theme}>
+                <Button
+                    variant="outlined"
+                    color="secondary"
+                    className={classes.button}
+                    endIcon={<ArrowForwardIosIcon />}
+                >
+                    Next
+        </Button>
+
+                <Button
+                    variant="outlined"
+                    color="secondary"
+                    className={classes.button}
+                    startIcon={<ArrowBackIosIcon />}
+                >
+                    Back
+        </Button>
+            </MuiThemeProvider>
+        </div>
+
+    );
+}
+
+
+{/*export default Variables3; */ }
