034abb09e5fabde996a08255063e95a22d572472
isabella <isabella.berryx3@gmail.com>
2020-08-02 22:34:07 -0400

    completed first page

--- src/pages/Lessons.js
+++ src/pages/Lessons.js
@@ -1,22 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Grid, Container, Paper, makeStyles } from '@material-ui/core/';
 import '../App.css';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}));
+
 export default function Lessons() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const classes = useStyles();
+
+    return (
+        <div className="Lessons">
+            <Container>
+                <Grid container spacing={10}>
+                    <Grid item xs={12}>
+                        <Paper>Technology</Paper>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Paper className={classes.paper}>
+                            <Link to="/">Science</Link>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Paper className={classes.paper}>
+                            <Link to="/tech/lessons">Technology</Link>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Paper className={classes.paper}>
+                            <Link to="/">Engineering</Link>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Paper className={classes.paper}>
+                            <Link to="/">Mathematics</Link>
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </Container>
+        </div>
+    );
 }
